44fd2ecc03399d081bd8e1ce7667dfe0ed11f7b7
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2023-12-05 17:04:25 -0500

    Avoiding Component execution with Clever structuring

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -27,7 +27,7 @@ function isPrime(number) {
   return true;
 }
 
-const Counter = memo( function Counter({ initialCount }) {
+export default function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
   const initialCountIsPrime = isPrime(initialCount);
 
@@ -58,7 +58,4 @@ const Counter = memo( function Counter({ initialCount }) {
       </p>
     </section>
   );
-}
-)
-
-export default Counter;
+  }
