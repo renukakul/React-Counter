68092f0907a56817dd7d1d20b419b02f47f8740e
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2023-12-05 17:44:42 -0500

    useMemo() Hook

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback ,useMemo} from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -29,7 +29,7 @@ function isPrime(number) {
 
 export default function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount),[]);
 
   const [counter, setCounter] = useState(initialCount);
 
