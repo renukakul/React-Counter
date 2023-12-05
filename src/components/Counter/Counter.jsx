16ebb2e9a798bf221b5f16af24fb443cd2469d52
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2023-12-05 16:21:55 -0500

    Added Memo() function

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -27,7 +27,7 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
+const Counter = memo( function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
   const initialCountIsPrime = isPrime(initialCount);
 
@@ -59,3 +59,6 @@ export default function Counter({ initialCount }) {
     </section>
   );
 }
+)
+
+export default Counter;
